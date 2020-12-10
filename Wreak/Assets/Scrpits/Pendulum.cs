239fe661b1b8287375728b5faca05005c8a1178c
Itzuzume <ellson637@gmail.com>
2020-12-10 18:27:04 +0800

    Pendulum

---
+++ Wreak/Assets/Scrpits/Pendulum.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+
+    [SerializeField, Range(0.0f, 360f)]
+    private float _angle = 90.0f;
+
+    [SerializeField, Range(0.0f, 5.0f)]
+    private float _speed = 2.0f;
+
+    Quaternion _start, _end;
+
+    [SerializeField, Range(0.0f, 10.0f)]
+    private float _startTime = 0.0f;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _start = PendulumRotation(_angle);
+        _end = PendulumRotation(-_angle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _startTime += Time.deltaTime;
+        transform.rotation = Quaternion.Lerp(_start, _end, (Mathf.Sin(_startTime * _speed + Mathf.PI / 2) + 1.0f) / 2.0f);
+    }
+
+
+
+
+    void ResetTimer()
+    {
+        _startTime = 0.0f;
+    }
+
+
+
+    Quaternion PendulumRotation(float angle)
+    {
+        var pendulumRoation = transform.rotation;
+        var anglez = pendulumRoation.eulerAngles.z + angle;
+
+        if(anglez> 180)
+        {
+            anglez -= 360;
+        }
+        else if(anglez < -180)
+        {
+            anglez += 360;
+        }
+
+        pendulumRoation.eulerAngles = new Vector3(pendulumRoation.eulerAngles.x, pendulumRoation.eulerAngles.y, anglez);
+        return pendulumRoation;
+    }
+}
---
+++ Wreak/Assets/Scrpits/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Verticalinput = Input.GetAxis("Vertical");
+        float Horizontalinput = Input.GetAxis("Horizontal");
+
+
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            StartMove();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            StartMove();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            StartMove();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            StartMove();
+        }
+
+    }
+
+    void StartMove()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+
+   
+}
